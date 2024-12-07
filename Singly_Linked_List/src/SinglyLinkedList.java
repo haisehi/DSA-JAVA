8e00ad09959b810654fa18e35cc4ba9eae7e1fd8
haisehi <88788629+haisehi@users.noreply.github.com>
2024-12-08 03:04:38 +0700

    singly liked list

--- Singly_Linked_List/src/SinglyLinkedList.java
+++ Singly_Linked_List/src/SinglyLinkedList.java
@@ -28,20 +28,94 @@ public class SinglyLinkedList {
         }
         return count;
     }
+    // Insert node at the beginning of a singly linked list
+    public void InsertNodeAtB(int value){
+        ListNode newNode = new ListNode(value);
+        newNode.next=head;
+        head=newNode;
+    }
+
+    // Insert a node in a Singly Linked List at a given position
+    public void Insert(int position, int value){
+        ListNode node = new ListNode(value);
+        if(position ==1){
+            node.next=head;
+            head = node;
+        }
+        else {
+            ListNode previous = head;
+            int count = 1;
+
+            while (count <position-1){
+                previous = previous.next;
+                count++;
+            }
+
+            ListNode current = previous.next;
+            previous.next = node;
+            node.next = current;
+        }
+    }
+
+    // Insert node at the end of a Singly Linked List
+    public void InsertNodeAtE(int value){
+        ListNode newNode = new ListNode(value);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        ListNode current = head;
+        while (null != current.next){
+            current = current.next;
+        }
+        current.next=newNode;
+    }
+
+    public ListNode deleteF(){
+        if(head == null){
+            return null;
+        }
+
+        ListNode temp = head;
+        head = head.next;
+        temp.next = null;
+
+        return temp;
+    }
 
     public static void main(String[] args) {
         SinglyLinkedList sll = new SinglyLinkedList();
         //Input data to head node
-        sll.head = new ListNode(10);
-        //Input data to other nodes
-        ListNode second = new ListNode(1);
-        ListNode third = new ListNode(8);
-        ListNode fourth = new ListNode(11);
-        //we will connect them together to form a chain
-        sll.head.next=second;//10-->1
-        second.next=third;//10-->1-->8
-        third.next=fourth;//10-->1-->8-->11-->null
+//        sll.head = new ListNode(10);
+//        //Input data to other nodes
+//        ListNode second = new ListNode(1);
+//        ListNode third = new ListNode(8);
+//        ListNode fourth = new ListNode(11);
+//        //we will connect them together to form a chain
+//        sll.head.next=second;//10-->1
+//        second.next=third;//10-->1-->8
+//        third.next=fourth;//10-->1-->8-->11-->null
+        // Insert
+        sll.InsertNodeAtB(10);
+        sll.InsertNodeAtB(1);
+        sll.InsertNodeAtB(8);
+        sll.InsertNodeAtB(11);
 
+        sll.InsertNodeAtE(9);
+        sll.InsertNodeAtE(7);
+
+        sll.Insert(1,4);
+        sll.Insert(3,5);
+        sll.Insert(9,14);
+
+        // Display
+        sll.display();
+        System.out.println("\nlength of a Singly Linked List: "+sll.countNode());
+
+        // Delete
+        System.out.println("Delete first node "+sll.deleteF().data);
+
+        // Display
         sll.display();
         System.out.println("\nlength of a Singly Linked List: "+sll.countNode());
     }
